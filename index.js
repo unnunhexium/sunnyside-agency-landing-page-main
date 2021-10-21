071e3bde267a0af3287f0b4a869b661955cef76c
kamila <kamila.lach1@gmail.com>
2021-10-21 19:18:37 +0200

    Final touch

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const hamburger = document.querySelector(".top__hamburger");
 const nav = document.querySelector(".mobile-nav");
-const heading = document.querySelector(".heading");
 const handleClick = () => {
   hamburger.classList.toggle("top__hamburger--active");
   nav.classList.toggle("mobile-nav--active");
-  heading.classList.toggle("heading--active");
 };
 
 hamburger.addEventListener("click", handleClick);
